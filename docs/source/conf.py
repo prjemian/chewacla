5e17cfd5c9bf50d1187aa53d3f01aa71ad1e5f07
Pete R Jemian <prjemian@users.noreply.github.com>
2025-09-05 11:05:25 -0500

    DOC wip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,13 +3,43 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# conda activate chewacla
+# ./docs/make.bat html
+
+# -- Path setup --------------------------------------------------------------
+
+import pathlib
+import sys
+import tomllib
+from importlib.metadata import version
+
+root_path = pathlib.Path(__file__).parent.parent.parent
+with open(root_path / "pyproject.toml", "rb") as fp:
+    toml = tomllib.load(fp)
+metadata = toml["project"]
+
+sys.path.insert(0, str(root_path))
+
+# imports here for sphinx to build the documents without many WARNINGS.
+import chewacla
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "chewacla"
-copyright = "2025, Pete R Jemian"
-author = "Pete R Jemian"
-release = "0.0.1"
+author = metadata["authors"][0]["name"]
+copyright = toml["tool"]["copyright"]["copyright"]
+description = metadata["description"]
+github_url = metadata["urls"]["source"]
+project = metadata["name"]
+release = chewacla.__version__
+rst_prolog = f".. |author| replace:: {author}"
+today_fmt = "%Y-%m-%d %H:%M"
+
+# -- Special handling for version numbers ------------------------------------
+# https://github.com/pypa/setuptools_scm#usage-from-sphinx
+
+release = version(project)
+version = ".".join(release.split(".")[:2])
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,6 +54,7 @@ exclude_patterns = []
 # AutoAPI configuration
 autoapi_type = "python"  # Specify the type of API to document
 autoapi_dirs = [f"../../src/{project}"]  # Path to the directory containing your code
+autoapi_excluded_paths = ["src/chewacla/tests/"]  # Exclude the tests directory
 autoapi_options = [
     "members",  # Include members (functions, classes, etc.)
     "undoc-members",  # Include undocumented members
@@ -38,5 +69,10 @@ autoapi_options = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-# html_title = f"{project} {version}"
+html_title = f"{project} {version}"
 html_static_path = ['_static']
+
+autodoc_mock_imports = """
+    h5py
+    numpy
+""".split()
