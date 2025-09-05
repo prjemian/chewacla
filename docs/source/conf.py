9c814e66b5441af44324e6297f3ad7cb1c485caa
Pete R Jemian <prjemian@users.noreply.github.com>
2025-09-05 10:37:28 -0500

    DOC configure autoapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,14 +14,29 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "autoapi.extension",  # Add sphinx-autoapi extension
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
+
+# AutoAPI configuration
+autoapi_type = "python"  # Specify the type of API to document
+autoapi_dirs = [f"../../src/{project}"]  # Path to the directory containing your code
+autoapi_options = [
+    "members",  # Include members (functions, classes, etc.)
+    "undoc-members",  # Include undocumented members
+    "private-members",  # Include private members
+    "special-members",  # Include special members (e.g., __init__)
+    "inherited-members",  # Include inherited members
+    "show-inheritance",  # Show inheritance diagrams
+]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_theme = "pydata_sphinx_theme"
+# html_title = f"{project} {version}"
+html_static_path = ['_static']
