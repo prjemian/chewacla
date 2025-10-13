8ae1c1facff7feac5956ac3d7ec764415cfd2c7d
Pete Jemian <prjemian@gmail.com>
2025-10-13 10:32:01 -0500

    MNT re-arrange

--- src/chewacla/tests/test_adhoc.py
+++ src/chewacla/tests/test_adhoc.py
@@ -11,62 +11,9 @@ from chewacla.adhoc import Chewacla
 from chewacla.adhoc import _AHLattice
 from chewacla.adhoc import expand_direction_map
 from chewacla.shorthand import DirectionShorthand
+from chewacla.tests.common import APS_FOURC_GEOMETRY
+from chewacla.tests.common import SILICON_LATTICE
 from chewacla.utils import rodrigues_rotation
-
-CUBIC_5A_LATTICE = 5, 5, 5, 90, 90, 90
-DOLOMITE_LATTICE = 4.8012, 4.8012, 16.002, 90, 90, 120
-EUPTIN4_EH1_VER_LATTICE = 4.542, 16.955, 7.389, 90, 90, 90
-SILICON_LATTICE = 5.4310196, 5.4310196, 5.4310196, 90, 90, 90
-
-APS_FOURC_GEOMETRY = dict(
-    # E4CV
-    incident_beam="z+",  # APS coordinate system
-    sample_stage=dict(omega="x-", chi="z+", phi="x+"),
-    detector_stage=dict(ttheta="x-"),
-)
-APS_PSIC_GEOMETRY = dict(
-    # psic, H. You
-    incident_beam="z+",  # APS coordinate system
-    sample_stage=dict(mu="y+", eta="x-", chi="z+", phi="x+"),
-    detector_stage=dict(nu="y+", delta="x-"),
-)
-APS_SIXC_GEOMETRY = dict(
-    # IUCr 6-circle, Lohmeier & Vlieg
-    incident_beam="z+",  # APS coordinate system
-    sample_stage=dict(alpha="y+", omega="x-", chi="z+", phi="x+"),
-    detector_stage=dict(alpha="y+", delta="x-", gamma="y+"),
-)
-E4CH_GEOMETRY = dict(
-    # https://people.debian.org/~picca/hkl/hkl.html#org800c866
-    incident_beam="x+",  # z+: anti-gravity
-    sample_stage=dict(omega="z+", chi="x+", phi="z+"),
-    detector_stage=dict(tth="z+"),
-)
-E4CV_GEOMETRY = dict(
-    # https://people.debian.org/~picca/hkl/hkl.html#org1a91260
-    incident_beam="x+",
-    sample_stage=dict(omega="y-", chi="x+", phi="y-"),
-    detector_stage=dict(tth="y-"),
-)
-E6C_GEOMETRY = dict(
-    # https://people.debian.org/~picca/hkl/hkl.html#orgf48ceba
-    incident_beam="x+",
-    sample_stage=dict(mu="z+", omega="y-", chi="x+", phi="y-"),
-    detector_stage=dict(gamma="z+", delta="y-"),
-)
-KAPPA_6_CIRCLE_GEOMETRY = dict(
-    incident_beam="x+",
-    # kappa angle: alpha = 50 degrees
-    sample_stage=dict(mu="z+", keta="y-", kappa=[0.0, -0.6427876096865394, -0.766044443118978], kphi="y-"),
-    detector_stage=dict(nu="z+", tth="y-"),
-)
-K4CV_GEOMETRY = dict(
-    # https://people.debian.org/~picca/hkl/hkl.html#org182695d
-    incident_beam="x+",
-    # kappa angle: alpha = 50 degrees
-    sample_stage=dict(komega="y-", kappa=[0.0, -0.6427876096865394, -0.766044443118978], kphi="y-"),
-    detector_stage=dict(tth="y-"),
-)
 
 
 @pytest.mark.parametrize(
